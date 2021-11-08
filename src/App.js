e26a94e78c22d519d19324563656e0ec49e34e32
Matheus Patrick <mpatrickaires@gmail.com>
2021-11-08 08:51:16 -0300

    Implementacao do Navbar e Footer e incluido a nova pagina Projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
     return (
         <Router>
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/company">Empresa</Link>
-                <Link to="/contact">Contato</Link>
-                <Link to="/newproject">Novo Projeto</Link>
-            </div>
+            <Navbar />
             <Switch>
                 <Container customClass="min-height">
                     <Route exact path="/">
                         <Home />
+                    </Route>
+                    <Route path="/projects">
+                        <Projects />
                     </Route>
                     <Route path="/company">
                         <Company />
@@ -32,7 +33,7 @@ function App() {
                     </Route>
                 </Container>
             </Switch>
-            <footer>Footer</footer>
+            <Footer />
         </Router>
     );
 }
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,33 @@
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
+                <li>
+                    <a
+                        href="https://www.linkedin.com/in/mpatrickaires/"
+                        target="_blank"
+                    >
+                        <FaLinkedin />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://www.github.com/mpatrickaires"
+                        target="_blank"
+                    >
+                        <FaGithub />
+                    </a>
+                </li>
+            </ul>
+            <p className={styles.copy_right}>
+                <span>Costs</span> &copy; 2021
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
