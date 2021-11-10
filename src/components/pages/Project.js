c7f39872ad971ce9f71e483ed4cc20c367daa38d
Matheus Patrick <mpatrickaires@gmail.com>
2021-11-10 08:51:03 -0300

    Realizado mais implementacoes na edicao de projetos

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 
 import Loading from '../layout/Loading';
 import Container from '../layout/Container';
+import Message from '../layout/Message';
+import ProjectForm from '../project/ProjectForm';
 
 import styles from './Project.module.css';
 
@@ -11,6 +13,8 @@ function Project() {
 
     const [project, setProject] = useState([]);
     const [showProjectForm, setShowProjectForm] = useState(false);
+    const [message, setMessage] = useState();
+    const [messageType, setMessageType] = useState();
 
     useEffect(() => {
         setTimeout(() => {
@@ -28,6 +32,32 @@ function Project() {
         }, 300);
     }, [id]);
 
+    function editPost(project) {
+        if (project.budget < project.cost) {
+            setMessage(
+                'O orçamento não pode ser menor que o custo do projeto!'
+            );
+            setMessageType('error');
+            return false;
+        }
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(data);
+                setShowProjectForm(false);
+                setMessage('Projeto atualizado!');
+                setMessageType('success');
+            })
+            .catch((err) => console.log(err));
+    }
+
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm);
     }
@@ -36,6 +66,7 @@ function Project() {
         <>
             {project.name ? (
                 <div className={styles.project_details}>
+                    {message && <Message type={messageType} msg={message} />}
                     <Container customClass="column">
                         <div className={styles.detail_container}>
                             <h1>Projeto: {project.name}</h1>
@@ -62,7 +93,11 @@ function Project() {
                                 </div>
                             ) : (
                                 <div className={styles.project_info}>
-                                    <p>form</p>
+                                    <ProjectForm
+                                        handleSubmit={editPost}
+                                        btnText="Concluir Edição"
+                                        projectData={project}
+                                    />
                                 </div>
                             )}
                         </div>
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -35,7 +35,7 @@ function ProjectForm({ handleSubmit, btnText, projectData }) {
 
     function handleCategory(e) {
         setProject({
-            ...project, 
+            ...project,
             category: {
                 id: e.target.value,
                 name: e.target.options[e.target.selectedIndex].text,
